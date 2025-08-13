f4266258d13d716121e707c078766340d0c19ff4
lazqii <gilangrizqi2016@gmail.com>
2025-08-13 13:48:30 +0700

    add quote model

---
+++ lib/models/quote.dart
@@ -0,0 +1,11 @@
+// Model Quote
+class Quote {
+  final String text;
+  final String author;
+
+  Quote({required this.text, required this.author});
+
+  factory Quote.fromJson(Map<String, dynamic> json) {
+    return Quote(text: json['q'], author: json['a']);
+  }
+}
